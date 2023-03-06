cf0a232afe8b03a14bb31617dc2f289545760363
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-06 13:42:11 -0300

    Added: Choice Serializer

---
+++ polls/serializers.py
@@ -0,0 +1,11 @@
+
+from rest_framework import serializers
+
+from .models import Choice
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+
