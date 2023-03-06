9e63101542c832594218dde2fed6c8bc91a6a5bb
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-06 14:08:57 -0300

    Added: API View

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,20 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.response import Response
 from .serializers import ChoiceSerializer
 from .models import Question, Choice
 from rest_framework.mixins import UpdateModelMixin
 from rest_framework.generics import GenericAPIView
+from rest_framework.decorators import api_view
 
+@api_view(http_method_names=['GET'])
 def polls_results(request,pk):
     queryset = Choice.objects.all().values('choice_text','votes').filter(question_id = pk) 
     query_list = list(queryset)
     return JsonResponse(query_list, safe=False, status=status.HTTP_200_OK)
 
+
+@api_view(http_method_names=['GET'])
 def polls_detail(request,pk):
     poll = get_object_or_404(Question, pk=pk)
     data = {
@@ -19,12 +22,13 @@ def polls_detail(request,pk):
         "pub_date": poll.pub_date
     }
     return JsonResponse(data, status=status.HTTP_200_OK)
-
+    
+@api_view(http_method_names=['GET'])
 def polls_list(request):
     data = list(Question.objects.all().values()[:20])
     return JsonResponse({
-                         'questions': data},
-                        safe=False, status=status.HTTP_200_OK)
+                            'questions': data},
+                            safe=False, status=status.HTTP_200_OK)
 
 class VoteView(GenericAPIView, UpdateModelMixin):
     queryset = Choice.objects.all()
