b9a8d68e82a9595ed2778e777126ed92cb74888e
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-08 21:52:00 -0300

    Added: Import Export to Admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import Choice, Question
 import adminactions.actions as actions
+from import_export.admin import ImportExportModelAdmin
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(ImportExportModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
