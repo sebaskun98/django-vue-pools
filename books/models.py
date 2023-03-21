6eb7923457fd3fec75ed060c989da2816e850124
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-21 14:44:24 -0300

    Added: Models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'author'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    pCompany = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author, related_name='books')
+
+    class Meta:
+        db_table = 'book'
+        ordering = ['name', 'authors']
+
+    def __str__(self):
+        return self.name
