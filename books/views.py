8d46edc65ecf4c48f51c96ea31901c1cb225fcfd
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-24 11:55:47 -0300

    Added: All Views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Author, Book, Category, Comment, PublishingCompany, Rating
+from .serializers import AuthorSerializer, BookSerializer, CategorySerializer, CommentSerializer, \
+    PublishingCompanySerializer, RatingSerializer
+
+
+class AuthorPagination(PageNumberPagination):
+    page_size = 10
+
+
+class AuthorListAPIView(ModelViewSet):
+    queryset = Author.objects.all()
+    pagination_class = AuthorPagination
+    serializer_class = AuthorSerializer
+    filter_backends = [DjangoFilterBackend]
+
+
+class BookViewSet(ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class CategoryViewSet(ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class PCompanyViewSet(ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = PublishingCompanySerializer
+    queryset = PublishingCompany.objects.all()
+
+
+class CommentViewSet(ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+
+class RatingViewSet(ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    serializer_class = RatingSerializer
+    queryset = Rating.objects.all()
