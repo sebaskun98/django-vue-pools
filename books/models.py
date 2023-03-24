5fdfb729c3e2464406b448851cde98f5ddd9bc3c
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-24 11:05:39 -0300

    Updated: New Models and Book with new FKs

--- books/models.py
+++ books/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey("Author", null=True, on_delete=models.DO_NOTHING)
+    publishingCompany = models.ForeignKey("PublishingCompany", null=True, on_delete=models.DO_NOTHING)
+    category = models.ForeignKey("Category", null=True, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'book'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -11,15 +26,31 @@ class Author(models.Model):
         return self.name
 
 
-class Book(models.Model):
-    name = models.CharField(max_length=100)
-    pCompany = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author, related_name='books')
-
-    class Meta:
-        db_table = 'book'
-        ordering = ['name']
+class PublishingCompany(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=255)
+    book_id = models.ForeignKey("Book", on_delete=models.DO_NOTHING)
+    rating = models.ForeignKey("Rating", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.comment
+
+
+class Rating(models.Model):
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.rating
