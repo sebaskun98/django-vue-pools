608b822332abe41eff6b2da62f1097154a5f20c3
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-24 11:07:56 -0300

    Added: Category and Company model admins

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.text import get_text_list
 
-from .models import Author, Book
+from .models import Author, Book, Category, PublishingCompany
 
 
 @admin.register(Author)
@@ -10,21 +10,33 @@ class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('id', 'name')
 
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+@admin.register(PublishingCompany)
+class PCompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'name', 'pCompany', 'category', 'authors_names',
+        'id', 'name', 'publishingCompany', 'category', 'author',
     )
     list_display_links = ('id', 'name',)
-    list_filter = ('pCompany', 'category',)
-    search_fields = ('id', 'name', 'authors__name',)
-    autocomplete_fields = ('authors',)
+    list_filter = ('publishingCompany', 'category',)
+    search_fields = ('id', 'name', 'author__name',)
+    autocomplete_fields = ('author', 'publishingCompany', 'category')
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        return qs.prefetch_related('authors')
+        return qs.prefetch_related('author')
 
-    @admin.display(description='authors')
+    @admin.display(description='author')
     def authors_names(self, obj):
         names = list(obj.authors.values_list('name', flat=True))
         return get_text_list(names, last_word='and')
