0e33c1bd0a575a03906deea872dabb359ae89f08
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-24 11:55:06 -0300

    Deleted: All URls

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import include, path
 
-from . import views
+from rest_framework import routers
+
+from .views import AuthorListAPIView, BookViewSet, PCompanyViewSet, RatingViewSet, CommentViewSet, CategoryViewSet
 
 app_name = 'books'
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+router.register('authors', AuthorListAPIView)
+router.register('pcompany', PCompanyViewSet)
+router.register('rating', RatingViewSet)
+router.register('comment', CommentViewSet)
+router.register('category', CategoryViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
 ]
